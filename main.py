5e9bba536f3a5608bc3bfd0d23cb16b0a75d790d
Cameron Steeves <76858431+dxzys@users.noreply.github.com>
2026-06-03 22:14:49 -0300

    Create main.py with initial layout
    Initialize main.py with imports and structure.

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests # we will need this to send web requests to the endpoints (retreive the data)
+
+# set constants for geocoder and NB hospital data API urls
+#   https://geocoder.ca/
+#     parameters: ?postal={postal_code}&geoit=XML&json=1
+#   https://services7.arcgis.com/BuriUAtfp8cZMlDt/arcgis/rest/services/dev_waittimes_exb_hfl_6da46_c5905_9256e/FeatureServer/0/query
+#     parameters: ?f=json&where=1%3D1&outFields=*&resultRecordCount=100&returnGeometry=true
+
+# function to calculate distance between two coordinates in kilometres (Haversine formula)
+
+# function to get coordinates from postal code
+
+# function to get hospital data
+
+# main function
+def main():
+  # code
+
+if __name__ == "__main__":
+    main()
