3b2b775d35de4da5f708e85a92523d1d5817ba32
sug221198-sudo <sug221198@gmail.com>
2026-06-04 22:39:36 -0300

    feat(unfinished): postal convert ability chore: concurrency of functions, code untested

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,25 @@ public class Main {
     public static void main(String[] args) {
         
         //INSTATIATE 
-        String inputPostalCode;
+        String userPostalCode;
         Scanner scanner = new Scanner(System.in);
 
-        //GET USER POSTAL CODE(CHECK FORMAT: LENGTH, INVALID)
+        //GET USER POSTAL CODE(CHECK FORMAT: LENGTH, INVALID, UPPERCASE)
         System.out.println("NB Hospital Distance Calculator\nEnter your postal code (format: A1A1A1): ");
-        inputPostalCode = scanner.nextLine();
+        userPostalCode = scanner.nextLine();
+        userPostalCode = userPostalCode.toUpperCase();
 
         //CALL FUNCTION TO GET COORDINATES FROM POSTAL CODE
-        
+        double[] userLocation = functions.PostalConverter.getLocation(userPostalCode);
+        while (userLocation == null) {
+            System.out.println("Invalid postal code. Please enter a valid postal code (format: A1A1A1): ");
+            userPostalCode = scanner.nextLine();
+            userPostalCode = userPostalCode.toUpperCase();
+            userLocation = functions.PostalConverter.getLocation(userPostalCode);
+        }
+        System.out.format("Your location: (%.6f, %.6f)\n", userLocation[0], userLocation[1]);
 
         //GET INSTATIATE HOSPITAL DATA
-        functions.HospitalData.loadHospitalData();
+        functions.HospitalData.loadHospitalData();//i think we need to use both user location and hospital data at the same time 
     }
 }
---
+++ src/functions/PostalConverter.java
@@ -0,0 +1,50 @@
+package functions;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+public class PostalConverter {
+    public PostalConverter() {}
+    public static double[] getLocation(String postalCode) {
+        
+        try{
+            //CREATE A URL
+            String baseUrl = "https://geocoder.ca/";
+            String parameters = "?postal={postal_code}&geoit=XML&json=1";
+            String url = baseUrl + parameters;
+
+            //INITIALIZE CLIENT
+            HttpClient client = HttpClient.newBuilder()
+                                      .connectTimeout(java.time.Duration.ofSeconds(10))
+                                      .build();
+
+            //CREATE A REQUEST
+            java.net.http.HttpRequest request = HttpRequest.newBuilder()
+                                                           .uri(URI.create(url))
+                                                           .header("User-Agent", "Mozilla/5.0(Macintosh; Intel Mac OS X 10_15_7)")
+                                                           .GET()
+                                                           .build();
+            // SEND THE REQUEST
+            System.out.println("accessing ArcGIS ");
+            HttpResponse<String> strResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // PRINT THE RESPONSE
+            if (strResponse.statusCode() == 200) {
+                JsonObject rootObj = JsonParser.parseString(strResponse.body()).getAsJsonObject();
+
+                double lantitude = rootObj.get("latt").getAsDouble();
+                double longitude = rootObj.get("longt").getAsDouble();
+                return new double[]{lantitude, longitude};
+            } else {
+                System.out.println("Error fetching location data: " + strResponse.statusCode());
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Error fetching location data: " + e.getMessage());
+        }
+        return null;
+    }
+}
