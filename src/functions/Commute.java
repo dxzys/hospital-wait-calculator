856b24477b442d54a868ff8507eaafd10c9597c8
苏哥，梦想哭了。 <sug221198@gmail.com>
2026-06-05 15:09:54 -0300

    Update Commute.java

--- src/functions/Commute.java
+++ src/functions/Commute.java
@@ -3,12 +3,12 @@ package functions;
 public class Commute {
     public Commute() {}
     //GET THE DISITANCE
-    public static double getDistance(double lantitude1, double longitude1, double lantitude2, double longitude2) {
+    public static double getDistance(double latitude1, double longitude1, double latitude2, double longitude2) {
             double R = 6371; 
-            double dLat = Math.toRadians(lantitude2 - lantitude1);
+            double dLat = Math.toRadians(latitude2 - latitude1);
             double dLon = Math.toRadians(longitude2 - longitude1);
             double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                        Math.cos(Math.toRadians(lantitude1)) * Math.cos(Math.toRadians(lantitude2)) *
+                        Math.cos(Math.toRadians(latitude1)) * Math.cos(Math.toRadians(latitude2)) *
                         Math.sin(dLon / 2) * Math.sin(dLon / 2);
             double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
             double distance = R * c; 
