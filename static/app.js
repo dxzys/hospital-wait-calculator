e40ee829790dd96665b456f7b75e64d65d66456f
sug221198-sudo <sug221198@gmail.com>
2026-06-12 12:23:50 +0800

    fix: static

--- static/app.js
+++ static/app.js
@@ -1,1 +1,9 @@
+const map = L.map('map').setView([45.273, -66.063], 13);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+
+hospitalMarker = L.marker([45.305, -66.084]).addTo(map);
+        hospitalMarker.bindPopup("<b>Saint John Regional Hconstospital</b><br>Loading....").openPopup();        
